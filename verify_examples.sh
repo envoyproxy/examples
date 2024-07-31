b90abf425eec301664ebc4d2b5d94d579dddcee6
phlax <phlax@users.noreply.github.com>
2024-07-31 14:43:53 +0100

    ci: Cleanups (#5)
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- verify_examples.sh
+++ verify_examples.sh
@@ -1,10 +1,23 @@
+#!/bin/bash -e
 
-RESULTS=()
-ERRORS=()
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+RETURNS=0
 
 trim () {
     text=$(< /dev/stdin)
     echo -n "$text" | sed 's/^[[:space:]]*//;s/[[:space:]]*$//'
+}
+
+pass () {
+    local timing="${1}"
+    echo "${GREEN}passed${NC} in ${timing}"
+}
+
+fail () {
+    local timing="${1}" exitcode="${2}"
+    echo "${RED}failed (${exitcode})${NC} in ${timing}"
 }
 
 for result in "$@"; do
@@ -12,14 +25,25 @@ for result in "$@"; do
     NAME=$(echo $RESULT | cut -d: -f1 | trim)
     EXITCODE=$(echo $RESULT | cut -d: -f2 | trim)
     if [[ $EXITCODE != 0 ]]; then
-        ERROR=$(tail -n +2 $result)
-        ERRORS+=($ERROR)
+        echo
+        echo -e "============= ${RED}FAILED (${NAME})${NC} ===================="
+        tail -n+3 $result
+        echo -e "============ ${RED}/FAILED (${NAME})${NC} ===================="
+        echo
     fi
-    RESULTS+=("$NAME: $EXITCODE")
 done
 
-# echo "${ERRORS[@]}"
-
-for result in "${RESULTS[@]}"; do
-    echo "$result"
+for result in "$@"; do
+    RESULT=$(head -n1 "$result")
+    NAME=$(echo $RESULT | cut -d: -f1 | trim)
+    EXITCODE=$(echo $RESULT | cut -d: -f2 | trim)
+    TIME=$(echo $RESULT | cut -d: -f3 | trim)
+    OUTCOME=$(pass "${TIME}")
+    if [[ $EXITCODE != 0 ]]; then
+        OUTCOME=$(fail "${TIME}" "$EXITCODE")
+        RETURNS=1
+    fi
+    echo -e "${NAME}: ${OUTCOME}"
 done
+
+exit "${RETURNS}"
