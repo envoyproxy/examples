5cf6eb750916a42a01eb719ead97dd763ce3f138
Damien Mehala <damien.mehala@datadoghq.com>
2024-08-24 12:38:10 +0200

    fix: source verify-common.sh path
    Signed-off-by: Damien Mehala <damien.mehala@datadoghq.com>

--- datadog-tracing/verify.sh
+++ datadog-tracing/verify.sh
@@ -3,7 +3,7 @@
 export NAME=datadog-tracing
 export PORT_PROXY="${DATADOG_PORT_PROXY:-10400}"
 
-# shellcheck source=examples/verify-common.sh
+# shellcheck source=verify-common.sh
 . "$(dirname "${BASH_SOURCE[0]}")/../verify-common.sh"
 
 run_log "Make a request to http service"
