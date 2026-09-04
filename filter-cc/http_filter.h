1dc9d183f63d32ae0d22c7c7d2290bc3ecbc3bbd
phlax <phlax@users.noreply.github.com>
2026-09-04 09:02:35 +0100

    examples/build: Add filter-cc (#1394)
    Signed-off-by: Ryan Northey <ryan@synca.io>

---
+++ filter-cc/http_filter.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <string>
+
+#include "source/extensions/filters/http/common/pass_through_filter.h"
+
+#include "http_filter.pb.h"
+
+namespace Envoy {
+namespace Http {
+
+class HttpSampleDecoderFilterConfig {
+public:
+  HttpSampleDecoderFilterConfig(const sample::Decoder& proto_config);
+
+  const std::string& key() const { return key_; }
+  const std::string& val() const { return val_; }
+
+private:
+  const std::string key_;
+  const std::string val_;
+};
+
+using HttpSampleDecoderFilterConfigSharedPtr = std::shared_ptr<HttpSampleDecoderFilterConfig>;
+
+class HttpSampleDecoderFilter : public PassThroughDecoderFilter {
+public:
+  HttpSampleDecoderFilter(HttpSampleDecoderFilterConfigSharedPtr);
+
+  // Http::StreamDecoderFilter
+  FilterHeadersStatus decodeHeaders(RequestHeaderMap&, bool) override;
+
+private:
+  const HttpSampleDecoderFilterConfigSharedPtr config_;
+
+  const LowerCaseString headerKey() const;
+  const std::string headerValue() const;
+};
+
+} // namespace Http
+} // namespace Envoy
